e3557168d31eaae8008ced5c8bcbaad02d5fa1cd
Andrew Robbins <andrewrobbins@hotmail.com>
2020-08-16 14:43:51 -0500

    wip

---
+++ DeepLearning/Implementation/Layers/NeuralNetwork/FullyConnected.cpp
@@ -0,0 +1,5 @@
+#include "DeepLearning/Implementation/Layers/NeuralNetwork/FullyConnected.h"
+
+const float FullyConnected::ALPHA_NO_SCALE = 1.0f;
+const float FullyConnected::BETA_CLEAR = 1.0f;
+const float FullyConnected::BETA_ACCUMULATE = 1.0f;
