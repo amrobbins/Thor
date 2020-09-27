8ff6ec826d675680713c570c8c20f39293fce862
Andrew Robbins <andrewrobbins@hotmail.com>
2020-09-27 16:50:09 -0500

    wip

--- DeepLearning/Api/Layers/Learning/Convolution2d.cpp
+++ DeepLearning/Api/Layers/Learning/Convolution2d.cpp
@@ -5,53 +5,70 @@ using namespace Thor;
 void Convolution2d::convertToSingleLayersAndAddToNetwork() {
     assert(isMultiLayer());
 
+    BatchNormalization::Builder batchNormBuilder;
+    if (useBatchNormalization) {
+        batchNormBuilder.network(*network);
+        if (batchNormExponentialRunningAverageFactor.isPresent())
+            batchNormBuilder.exponentialRunningAverageFactor(batchNormExponentialRunningAverageFactor.get());
+        if (batchNormEpsilon.isPresent())
+            batchNormBuilder.epsilon(batchNormEpsilon.get());
+    }
+
+    Convolution2d::Builder convolution2dBuilder;
+    convolution2dBuilder.network(*network)
+        .numOutputChannels(numOutputChannels)
+        .filterHeight(filterHeight)
+        .filterWidth(filterWidth)
+        .verticalStride(verticalStride)
+        .horizontalStride(horizontalStride)
+        .verticalPadding(verticalPadding)
+        .horizontalPadding(horizontalPadding)
+        .hasBias(hasBias)
+        .weightsInitializer(weightsInitializer)
+        .biasInitializer(biasInitializer)
+        .noActivation();
+
+    vector<Tensor> currentFeatureInputs;
+
+    for (uint32_t i = 0; i < featureInputs.size(); ++i)
+        currentFeatureInputs.push_back(featureInputs[i]);
+
+    if (useBatchNormalization) {
+        for (uint32_t i = 0; i < featureInputs.size(); ++i) {
+            batchNormBuilder.featureInput(currentFeatureInputs[i]);
+        }
+        BatchNormalization batchNormalization = batchNormBuilder.build();
+        for (uint32_t i = 0; i < featureInputs.size(); ++i)
+            currentFeatureInputs[i] = batchNormalization.getFeatureOutputs()[i];
+    }
+
+    if (dropProportion > 0.0f) {
+        for (uint32_t i = 0; i < featureInputs.size(); ++i) {
+            DropOut dropOut =
+                DropOut::Builder().network(*network).dropProportion(dropProportion).featureInput(currentFeatureInputs[i]).build();
+            currentFeatureInputs[i] = dropOut.getFeatureOutput();
+        }
+    }
+
+    for (uint32_t i = 0; i < featureInputs.size(); ++i)
+        convolution2dBuilder.featureInput(currentFeatureInputs[i]);
+    Convolution2d convolution2d = convolution2dBuilder.build();
+    for (uint32_t i = 0; i < featureInputs.size(); ++i)
+        currentFeatureInputs[i] = convolution2d.getFeatureOutputs()[i];
+
+    if (activationBuilder) {
+        for (uint32_t i = 0; i < featureInputs.size(); ++i) {
+            activationBuilder->network(*network);
+            activationBuilder->featureInput(currentFeatureInputs[i]);
+            shared_ptr<Layer> activation = activationBuilder->build();
+            currentFeatureInputs[i] = activation->getFeatureOutput();
+        }
+    }
+
+    // Replace the outputs on the compound layer to be the outputs of the last stage
     for (uint32_t i = 0; i < featureInputs.size(); ++i) {
-        Tensor currentFeatureInput = featureInputs[i];
-
-        if (useBatchNormalization) {
-            BatchNormalization::Builder builder;
-            builder.network(*network);
-            if (batchNormExponentialRunningAverageFactor.isPresent())
-                builder.exponentialRunningAverageFactor(batchNormExponentialRunningAverageFactor.get());
-            if (batchNormEpsilon.isPresent())
-                builder.epsilon(batchNormEpsilon.get());
-            BatchNormalization batchNormalization = builder.featureInput(currentFeatureInput).build();
-            currentFeatureInput = batchNormalization.getFeatureOutput();
-        }
-
-        if (dropProportion > 0.0f) {
-            DropOut dropOut = DropOut::Builder().network(*network).dropProportion(dropProportion).featureInput(currentFeatureInput).build();
-            currentFeatureInput = dropOut.getFeatureOutput();
-        }
-
-        Convolution2d onlyConvolution = Builder()
-                                            .network(*network)
-                                            .featureInput(currentFeatureInput)
-                                            .numOutputChannels(numOutputChannels)
-                                            .filterHeight(filterHeight)
-                                            .filterWidth(filterWidth)
-                                            .verticalStride(verticalStride)
-                                            .horizontalStride(horizontalStride)
-                                            .verticalPadding(verticalPadding)
-                                            .horizontalPadding(horizontalPadding)
-                                            .hasBias(hasBias)
-                                            .weightsInitializer(weightsInitializer)
-                                            .biasInitializer(biasInitializer)
-                                            .activationBuilder(Optional<Activation::Builder>::empty())
-                                            .build();
-        currentFeatureInput = onlyConvolution.getFeatureOutput();
-        /*
-                if (activationBuilder.isPresent()) {
-                    activationBuilder.get().network(*network);
-                    activationBuilder.get().featureInput(currentFeatureInputs[i]);
-                    shared_ptr<Layer> activation = activationBuilder->build();
-                    currentFeatureInput = activation->getFeatureOutput();
-                }
-        */
-
-        Tensor finalFeatureOutput = currentFeatureInput;
-        featureOutputs.push_back(finalFeatureOutput);
-        outputTensorFromInputTensor[featureInputs[i]] = finalFeatureOutput;
-        inputTensorFromOutputTensor[finalFeatureOutput] = featureInputs[i];
+        convolution2d.featureOutputs[i] = currentFeatureInputs[i];
+        outputTensorFromInputTensor[featureInputs[i]] = featureOutputs[i];
+        inputTensorFromOutputTensor[featureOutputs[i]] = featureInputs[i];
     }
 }
--- test/DeepLearning/Api/Layers/Learning/FullyConnectedTest.cpp
+++ test/DeepLearning/Api/Layers/Learning/FullyConnectedTest.cpp
@@ -11,25 +11,13 @@ using std::shared_ptr;
 
 using namespace Thor;
 
-/*
-    virtual FullyConnected::Builder &network(Network &_network) {
-    virtual FullyConnected::Builder featureInput(Tensor _featureInput) {
-    virtual FullyConnected::Builder numOutputFeatures(uint32_t _numOutputFeatures) {
-    virtual FullyConnected::Builder hasBias(bool _hasBias) {
-    virtual FullyConnected::Builder weightsInitializer(Initializer _weightsInitializer) {
-    virtual FullyConnected::Builder biasInitializer(Initializer _biasInitializer) {
-    virtual FullyConnected::Builder activation(Optional<Activation> _activation) {
-    virtual FullyConnected::Builder batchNormalization(Optional<double> exponentialRunningAverageFactor = Optional<double>::empty(),
-   Optional<double> epsilon = Optional<double>::empty()) { virtual FullyConnected::Builder dropOut(float _dropProportion) {
-*/
-
 TEST(FullyConnectedSingleFeatureInput, Builds) {
     srand(time(nullptr));
 
     Network network;
 
     vector<uint64_t> dimensions;
-    int numDimensions = 1 + rand() % 6;
+    int numDimensions = 1;
     for (int i = 0; i < numDimensions; ++i)
         dimensions.push_back(1 + (rand() % 1000));
 
@@ -104,7 +92,7 @@ TEST(FullyConnectedMultipleFeatureInputs, Builds) {
     Network network;
 
     vector<uint64_t> dimensions;
-    int numDimensions = 1 + rand() % 6;
+    int numDimensions = 1;
     for (int i = 0; i < numDimensions; ++i)
         dimensions.push_back(1 + (rand() % 1000));
 
@@ -194,113 +182,6 @@ TEST(FullyConnectedMultipleFeatureInputs, Builds) {
     ASSERT_FALSE(fullyConnected > *clone);
     ASSERT_FALSE(fullyConnected < *clone);
 }
-
-/*
-
-TEST(FullyConnectedMultipleFeatureInputs, Builds) {
-    srand(time(nullptr));
-
-    Network network;
-
-    vector<uint64_t> dimensions;
-    int numDimensions0 = 1 + rand() % 6;
-    for (int i = 0; i < numDimensions0; ++i)
-        dimensions.push_back(1 + (rand() % 1000));
-    Tensor::DataType dataType = rand() % 2 ? Tensor::DataType::FP32 : Tensor::DataType::FP16;
-    Tensor featureInput0(dataType, dimensions);
-    Tensor featureInput1(dataType, dimensions);
-
-    double exponentialRunningAverageFactor = (1 + (rand() % 100)) / 1000.0f;
-
-    double epsilon = (1 + (rand() % 100)) / 100000.0f;
-
-    FullyConnected batchNormalization = FullyConnected::Builder()
-                                                .network(network)
-                                                .featureInput(featureInput0)
-                                                .featureInput(featureInput1)
-                                                .exponentialRunningAverageFactor(exponentialRunningAverageFactor)
-                                                .epsilon(epsilon)
-                                                .build();
-
-    ASSERT_TRUE(batchNormalization.isInitialized());
-
-    vector<Tensor> featureInputs = batchNormalization.getFeatureInputs();
-    vector<Tensor> featureOutputs = batchNormalization.getFeatureOutputs();
-    assert(featureInputs[0] == featureInput0);
-    assert(featureInputs[1] == featureInput1);
-
-    ASSERT_EQ(batchNormalization.getFeatureOutput(featureInput0), featureOutputs[0]);
-    ASSERT_EQ(batchNormalization.getFeatureOutput(featureInput1), featureOutputs[1]);
-    ASSERT_NE(featureOutputs[0].getId(), featureOutputs[1].getId());
-
-    assert(batchNormalization.getFeatureInput(featureOutputs[1]) == featureInputs[1]);
-    assert(batchNormalization.getFeatureInput(featureOutputs[0]) == featureInputs[0]);
-
-    ASSERT_EQ(featureInputs[0].getDataType(), dataType);
-    ASSERT_EQ(featureInputs[0].getDimensions(), dimensions);
-
-    ASSERT_EQ(featureInputs[1].getDataType(), dataType);
-    ASSERT_EQ(featureInputs[1].getDimensions(), dimensions);
-
-    ASSERT_EQ(featureOutputs[0].getDataType(), dataType);
-    ASSERT_EQ(featureOutputs[0].getDimensions(), dimensions);
-
-    ASSERT_EQ(featureOutputs[1].getDataType(), dataType);
-    ASSERT_EQ(featureOutputs[1].getDimensions(), dimensions);
-
-    double actualExponentialRunningAverageFactor = batchNormalization.getExponentialRunningAverageFactor();
-    ASSERT_EQ(actualExponentialRunningAverageFactor, exponentialRunningAverageFactor);
-
-    double actualEpsilon = batchNormalization.getEpsilon();
-    ASSERT_EQ(actualEpsilon, epsilon);
-
-    shared_ptr<Layer> cloneLayer = batchNormalization.clone();
-    FullyConnected *clone = dynamic_cast<FullyConnected *>(cloneLayer.get());
-    assert(clone != nullptr);
-
-    ASSERT_TRUE(clone->isInitialized());
-
-    featureInputs.clear();
-    featureOutputs.clear();
-    featureInputs = clone->getFeatureInputs();
-    featureOutputs = clone->getFeatureOutputs();
-    assert(featureInputs[0] == featureInput0);
-    assert(featureInputs[1] == featureInput1);
-
-    ASSERT_EQ(batchNormalization.getFeatureOutput(featureInput0), featureOutputs[0]);
-    ASSERT_EQ(batchNormalization.getFeatureOutput(featureInput1), featureOutputs[1]);
-    ASSERT_NE(featureOutputs[0].getId(), featureOutputs[1].getId());
-
-    assert(batchNormalization.getFeatureInput(featureOutputs[1]) == featureInputs[1]);
-    assert(batchNormalization.getFeatureInput(featureOutputs[0]) == featureInputs[0]);
-
-    ASSERT_EQ(featureInputs[0].getDataType(), dataType);
-    ASSERT_EQ(featureInputs[0].getDimensions(), dimensions);
-
-    ASSERT_EQ(featureInputs[1].getDataType(), dataType);
-    ASSERT_EQ(featureInputs[1].getDimensions(), dimensions);
-
-    ASSERT_EQ(featureOutputs[0].getDataType(), dataType);
-    ASSERT_EQ(featureOutputs[0].getDimensions(), dimensions);
-
-    ASSERT_EQ(featureOutputs[1].getDataType(), dataType);
-    ASSERT_EQ(featureOutputs[1].getDimensions(), dimensions);
-
-    double cloneExponentialRunningAverageFactor = clone->getExponentialRunningAverageFactor();
-    ASSERT_EQ(cloneExponentialRunningAverageFactor, exponentialRunningAverageFactor);
-
-    double cloneEpsilon = clone->getEpsilon();
-    ASSERT_EQ(cloneEpsilon, epsilon);
-
-    ASSERT_EQ(batchNormalization.getId(), clone->getId());
-    ASSERT_GT(batchNormalization.getId(), 1u);
-
-    ASSERT_TRUE(batchNormalization == *clone);
-    ASSERT_FALSE(batchNormalization != *clone);
-    ASSERT_FALSE(batchNormalization > *clone);
-    ASSERT_FALSE(batchNormalization < *clone);
-}
-*/
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
