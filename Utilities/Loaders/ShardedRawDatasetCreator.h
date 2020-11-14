55d5d61e0c7596811d4880a338fbadc61f6f841f
Andrew Robbins <andrewrobbins@hotmail.com>
2020-11-13 19:03:33 -0600

    wip

--- Utilities/Loaders/ShardedRawDatasetCreator.h
+++ Utilities/Loaders/ShardedRawDatasetCreator.h
@@ -5,11 +5,13 @@
 //       https://github.com/NVIDIA/DALI#compiling-dali-from-source-bare-metal
 //       Will need a C++ api, currently that is experimental. Will need to talk to nVidia.
 
+#include "Utilities/Common/Stream.h"
 #include "Utilities/WorkQueue/AsyncQueue.h"
 #include "Utilities/WorkQueue/WorkQueueUnordered.h"
 
 #include <boost/filesystem.hpp>
 #include <boost/interprocess/containers/map.hpp>
+#include <boost/interprocess/containers/vector.hpp>
 #include <boost/interprocess/managed_mapped_file.hpp>
 #include "omp.h"
 
@@ -31,15 +33,150 @@ using std::mutex;
 using std::string;
 using std::thread;
 
+typedef boost::interprocess::allocator<uint8_t, boost::interprocess::managed_mapped_file::segment_manager> file_vector_allocator_t;
+typedef boost::interprocess::vector<uint8_t, file_vector_allocator_t> file_vector_t;
+
 enum class ExampleType { TRAIN = 3, VALIDATE, TEST };
 
 struct DataElement {
     ExampleType exampleType;
     string className;
-    boost::filesystem::path destShard;
+    uint32_t destShard;
 
     uint64_t numDataBytes;
-    std::shared_ptr<char> data;
+    std::shared_ptr<uint8_t> data;
+};
+
+struct ShardMetadata {
+    uint64_t exampleSizeInBytes;
+};
+
+class Shard {
+   public:
+    Shard() {}
+
+    void createShard(
+        string filename, uint64_t numTrainExamples, uint64_t numValidateExamples, uint64_t numTestExamples, uint64_t exampleSizeInBytes) {
+        // FIXME: example class
+
+        this->filename = filename;
+        this->exampleSizeInBytes = exampleSizeInBytes;
+        uint64_t shardSizeInBytes = (numTrainExamples + numValidateExamples + numTestExamples) * exampleSizeInBytes + 1000000;
+        mappedFile = boost::interprocess::managed_mapped_file(boost::interprocess::create_only, filename.c_str(), shardSizeInBytes);
+
+        file_vector_allocator_t trainDataAllocator(mappedFile.get_segment_manager());
+        trainData = mappedFile.construct<file_vector_t>("train")(trainDataAllocator);
+        trainData->reserve(numTrainExamples * exampleSizeInBytes);
+
+        file_vector_allocator_t validateDataAllocator(mappedFile.get_segment_manager());
+        validateData = mappedFile.construct<file_vector_t>("validate")(validateDataAllocator);
+        validateData->reserve(numValidateExamples * exampleSizeInBytes);
+
+        file_vector_allocator_t testDataAllocator(mappedFile.get_segment_manager());
+        testData = mappedFile.construct<file_vector_t>("test")(testDataAllocator);
+        testData->reserve(numTestExamples * exampleSizeInBytes);
+
+        shardMetadata = mappedFile.construct<ShardMetadata>("shardMetadata")();
+        shardMetadata->exampleSizeInBytes = exampleSizeInBytes;
+    }
+
+    void openShard(string filename) {
+        mappedFile = boost::interprocess::managed_mapped_file(boost::interprocess::open_only, filename.c_str());
+        trainData = mappedFile.find<file_vector_t>("train").first;
+        validateData = mappedFile.find<file_vector_t>("validate").first;
+        testData = mappedFile.find<file_vector_t>("test").first;
+        shardMetadata = mappedFile.find<ShardMetadata>("shardMetadata").first;
+        exampleSizeInBytes = shardMetadata->exampleSizeInBytes;
+
+        assert(trainData->size() % exampleSizeInBytes == 0);
+        assert(testData->size() % exampleSizeInBytes == 0);
+        assert(validateData->size() % exampleSizeInBytes == 0);
+    }
+
+    void writeExample(uint8_t *buffer, ExampleType exampleType) {
+        assert(buffer != nullptr);
+
+        if (exampleType == ExampleType::TRAIN) {
+            assert(trainData->capacity() > trainData->size() + exampleSizeInBytes);
+            trainData->insert(trainData->end(), buffer, buffer + exampleSizeInBytes);
+        } else if (exampleType == ExampleType::VALIDATE) {
+            assert(validateData->capacity() > validateData->size() + exampleSizeInBytes);
+            validateData->insert(validateData->end(), buffer, buffer + exampleSizeInBytes);
+        } else if (exampleType == ExampleType::TEST) {
+            assert(testData->capacity() > testData->size() + exampleSizeInBytes);
+            testData->insert(testData->end(), buffer, buffer + exampleSizeInBytes);
+        } else {
+            assert(false);
+        }
+    }
+
+    void loadExample(uint8_t *buffer, ExampleType exampleType, uint64_t exampleIndex) {
+        assert(buffer != nullptr);
+
+        uint8_t *data;
+        if (exampleType == ExampleType::TRAIN) {
+            uint64_t numExamples = trainData->size() / exampleSizeInBytes;
+            assert(exampleIndex < numExamples);
+            data = trainData->data();
+        } else if (exampleType == ExampleType::VALIDATE) {
+            uint64_t numExamples = validateData->size() / exampleSizeInBytes;
+            assert(exampleIndex < numExamples);
+            data = validateData->data();
+        } else if (exampleType == ExampleType::TEST) {
+            uint64_t numExamples = testData->size() / exampleSizeInBytes;
+            assert(exampleIndex < numExamples);
+            data = testData->data();
+        } else {
+            assert(false);
+        }
+
+        uint8_t *exampleStart = data + (exampleIndex * exampleSizeInBytes);
+        memcpy(buffer, exampleStart, exampleSizeInBytes);
+    }
+
+    void loadExampleAsync(uint8_t *buffer, ExampleType exampleType, uint64_t exampleIndex, Stream stream) {
+        assert(buffer != nullptr);
+        cudaError_t cudaStatus;
+
+        uint8_t *data;
+        if (exampleType == ExampleType::TRAIN) {
+            uint64_t numExamples = trainData->size() / exampleSizeInBytes;
+            assert(exampleIndex < numExamples);
+            data = trainData->data();
+        } else if (exampleType == ExampleType::VALIDATE) {
+            uint64_t numExamples = validateData->size() / exampleSizeInBytes;
+            assert(exampleIndex < numExamples);
+            data = validateData->data();
+        } else if (exampleType == ExampleType::TEST) {
+            uint64_t numExamples = testData->size() / exampleSizeInBytes;
+            assert(exampleIndex < numExamples);
+            data = testData->data();
+        } else {
+            assert(false);
+        }
+
+        uint8_t *exampleStart = data + (exampleIndex * exampleSizeInBytes);
+        cudaStatus = cudaMemcpyAsync(buffer, exampleStart, exampleSizeInBytes, cudaMemcpyHostToHost, stream);
+        assert(cudaStatus == cudaSuccess);
+    }
+
+    void shrinkToFit() {
+        trainData->shrink_to_fit();
+        validateData->shrink_to_fit();
+        testData->shrink_to_fit();
+        bool status = boost::interprocess::managed_mapped_file::shrink_to_fit(filename.c_str());
+        assert(status == true);
+    }
+
+   private:
+    string filename;
+    uint64_t exampleSizeInBytes;
+    boost::interprocess::managed_mapped_file mappedFile;
+
+    file_vector_t *trainData;
+    file_vector_t *validateData;
+    file_vector_t *testData;
+    ShardMetadata *shardMetadata;
 };
 
 class DataProcessor : public WorkQueueExecutorBase<DataElement, DataElement> {
@@ -72,6 +209,7 @@ class ShardedRawDatasetCreator {
     uint64_t numTrainExamples;
     uint64_t numValidateExamples;
     uint64_t numTestExamples;
+    uint64_t outputTensorSizeInBytes;
 
     std::unordered_set<string> classes;
     // sourceDirectory -> (class -> [examplePath, examplePath, ...])
@@ -81,10 +219,11 @@ class ShardedRawDatasetCreator {
 
     uint32_t numOutputShards;
     std::vector<boost::filesystem::path> destShardFiles;
+    std::vector<Shard> shards;
 
     mutex mtx;
 
-    uint64_t getNumExamples();
+    void getNumExamples(uint64_t &numTrainExamples, uint64_t &numValidateExamples, uint64_t &numTestExamples);
     void loadExamples(WorkQueueUnordered<DataElement, DataElement> &workQueue);
     void writeDataToShard(WorkQueueUnordered<DataElement, DataElement> *workQueue);
 };
