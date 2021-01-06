2559ee47b492217f8274c7d2aa077e1a1ab74de9
Andrew Robbins <andrewrobbins@hotmail.com>
2021-01-06 17:51:38 -0600

    wip

--- test/Utilities/Loaders/ShardedRawDatasetCreatorTest.cpp
+++ test/Utilities/Loaders/ShardedRawDatasetCreatorTest.cpp
@@ -86,6 +86,9 @@ void verifyBatch(uint64_t batchSize, vector<ThorImplementation::Tensor> batchTen
     ASSERT_GT(batchSize, 0u);
     ASSERT_GT(numMiniBatches, 0u);
 
+    uint64_t globalCount = 0;
+    map<string, uint64_t> colorCount;
+
     for (uint64_t i = 0; i < batchTensors.size(); ++i) {
         ASSERT_EQ(batchTensors[i].getDescriptor().getTotalNumElements(), batchSize * 224 * 224 * 3);
         ASSERT_EQ(labelTensors[i].getDescriptor().getTotalNumElements(), batchSize * NUM_CLASSES);
@@ -101,53 +104,57 @@ void verifyBatch(uint64_t batchSize, vector<ThorImplementation::Tensor> batchTen
             }
             ASSERT_EQ(oneHotFound, true);
 
-            printf("%ld %ld\n", i, j);
-
             string imageColor = "";
             uint8_t *image = (uint8_t *)batchTensors[i].getMemPtr();
             image += j * 224 * 224 * 3;
-            if (image[0] == 255 && image[1] == 255 && image[2] == 255) {
+            if (image[0] == 255 && image[224 * 224] == 255 && image[2 * 224 * 224] == 255) {
                 imageColor = "white";
-                printf("%s-\n", imageColor.c_str());
                 for (int k = 0; k < 224 * 224; ++k) {
-                    ASSERT_EQ(image[3 * k], 255);
-                    ASSERT_EQ(image[3 * k + 1], 255);
-                    ASSERT_EQ(image[3 * k + 2], 255);
+                    ASSERT_EQ(image[k], 255);
+                    ASSERT_EQ(image[224 * 224 + k], 255);
+                    ASSERT_EQ(image[2 * 224 * 224 + k], 255);
                 }
-            } else if (image[0] == 0 && image[1] == 0 && image[2] == 0) {
+            } else if (image[0] == 0 && image[224 * 224] == 0 && image[2 * 224 * 224] == 0) {
                 imageColor = "black";
-                printf("%s-\n", imageColor.c_str());
                 for (int k = 0; k < 224 * 224; ++k) {
-                    ASSERT_EQ(image[3 * k], 0);
-                    ASSERT_EQ(image[3 * k + 1], 0);
-                    ASSERT_EQ(image[3 * k + 2], 0);
+                    ASSERT_EQ(image[k], 0);
+                    ASSERT_EQ(image[224 * 224 + k], 0);
+                    ASSERT_EQ(image[2 * 224 * 224 + k], 0);
                 }
-            } else if (image[0] == 255 && image[1] == 0 && image[2] == 0) {
+            } else if (image[0] == 255 && image[224 * 224] == 0 && image[2 * 224 * 224] == 0) {
                 imageColor = "red";
-                printf("%s-\n", imageColor.c_str());
                 for (int k = 0; k < 224 * 224; ++k) {
-                    ASSERT_EQ(image[3 * k], 255);
-                    ASSERT_EQ(image[3 * k + 1], 0);
-                    ASSERT_EQ(image[3 * k + 2], 0);
+                    ASSERT_EQ(image[k], 255);
+                    ASSERT_EQ(image[224 * 224 + k], 0);
+                    ASSERT_EQ(image[2 * 224 * 224 + k], 0);
                 }
-            } else if (image[0] == 0 && image[1] == 255 && image[2] == 0) {
+            } else if (image[0] == 0 && image[224 * 224] == 255 && image[2 * 224 * 224] == 0) {
                 imageColor = "green";
-                printf("%s-\n", imageColor.c_str());
                 for (int k = 0; k < 224 * 224; ++k) {
-                    ASSERT_EQ(image[3 * k], 0);
-                    ASSERT_EQ(image[3 * k + 1], 255);
-                    ASSERT_EQ(image[3 * k + 2], 0);
+                    ASSERT_EQ(image[k], 0);
+                    ASSERT_EQ(image[224 * 224 + k], 255);
+                    ASSERT_EQ(image[2 * 224 * 224 + k], 0);
                 }
-            } else if (image[0] == 0 && image[1] == 0 && image[2] == 255) {
+            } else if (image[0] == 0 && image[224 * 224] == 0 && image[2 * 224 * 224] == 255) {
                 imageColor = "blue";
-                printf("%s-\n", imageColor.c_str());
                 for (int k = 0; k < 224 * 224; ++k) {
-                    ASSERT_EQ(image[3 * k], 0);
-                    ASSERT_EQ(image[3 * k + 1], 0);
-                    ASSERT_EQ(image[3 * k + 2], 255);
+                    ASSERT_EQ(image[k], 0);
+                    ASSERT_EQ(image[224 * 224 + k], 0);
+                    ASSERT_EQ(image[2 * 224 * 224 + k], 255);
                 }
             }
             ASSERT_GT(imageColor.size(), 0u);
+
+            globalCount += 1;
+            colorCount[imageColor] += 1;
+            if (globalCount % 7 == 0) {
+                ASSERT_EQ(colorCount["white"], 1u);
+                ASSERT_EQ(colorCount["green"], 1u);
+                ASSERT_EQ(colorCount["red"], 1u);
+                ASSERT_EQ(colorCount["black"], 2u);
+                ASSERT_EQ(colorCount["blue"], 2u);
+                colorCount.clear();
+            }
         }
     }
 }
@@ -163,7 +170,6 @@ void verifyBatchAssembler(std::vector<shared_ptr<Shard>> shards) {
         ThorImplementation::TensorDescriptor(ThorImplementation::TensorDescriptor::DataType::UINT8, {3, 224, 224}),
         batchSize);
 
-    printf("batchSize %ld\n", batchSize);
     ASSERT_EQ(batchAssembler.getNumBatchesPerEpoch(), (7 + (batchSize - 1)) / batchSize);
 
     ThorImplementation::Tensor batchTensor;
@@ -173,12 +179,15 @@ void verifyBatchAssembler(std::vector<shared_ptr<Shard>> shards) {
     uint64_t batchNum;
 
     batchAssembler.getBatch(batchTensor, labelTensor, batchNum);
-    batchTensors.push_back(batchTensor);
-    labelTensors.push_back(labelTensor);
     do {
-        batchAssembler.getBatch(batchTensor, labelTensor, batchNum);
         batchTensors.push_back(batchTensor);
         labelTensors.push_back(labelTensor);
+        batchAssembler.getBatch(batchTensor, labelTensor, batchNum);
+    } while (batchNum != 1);
+    do {
+        batchTensors.push_back(batchTensor);
+        labelTensors.push_back(labelTensor);
+        batchAssembler.getBatch(batchTensor, labelTensor, batchNum);
     } while (batchNum != 1);
 
     verifyBatch(batchSize, batchTensors, labelTensors);
