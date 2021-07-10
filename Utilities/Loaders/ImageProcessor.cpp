1a5835d195e3f405d89148a75f8f9516b824add1
Andrew Robbins <andrewrobbins@hotmail.com>
2021-07-10 16:19:06 -0500

    wip

--- Utilities/Loaders/ImageProcessor.cpp
+++ Utilities/Loaders/ImageProcessor.cpp
@@ -2,21 +2,59 @@
 
 using namespace std;
 
+ImageProcessor::ImageProcessor(
+    double minAspectRatio, double maxAspectRatio, uint32_t outputImageRows, uint32_t outputImageColumns, bool display) {
+    this->minAspectRatio = minAspectRatio;
+    this->maxAspectRatio = maxAspectRatio;
+    this->outputImageRows = outputImageRows;
+    this->outputImageColumns = outputImageColumns;
+    this->display = display;
+    customProcessorUint8 = nullptr;
+    customProcessorHalf = nullptr;
+    bytesPerPixel = 1;
+}
+
 ImageProcessor::ImageProcessor(double minAspectRatio,
                                double maxAspectRatio,
                                uint32_t outputImageRows,
                                uint32_t outputImageColumns,
-                               bool (*customProcessor)(Magick::Image &regularlyProcessedImage),
+                               bool (*customProcessor)(uint8_t *regularlyProcessedImage),
                                bool display) {
     this->minAspectRatio = minAspectRatio;
     this->maxAspectRatio = maxAspectRatio;
     this->outputImageRows = outputImageRows;
     this->outputImageColumns = outputImageColumns;
-    this->customProcessor = customProcessor;
     this->display = display;
+    customProcessorUint8 = customProcessor;
+    customProcessorHalf = nullptr;
+    bytesPerPixel = 1;
 }
 
-uint64_t ImageProcessor::outputTensorSizeInBytes() { return 3 * outputImageRows * outputImageColumns; }
+ImageProcessor::ImageProcessor(double minAspectRatio,
+                               double maxAspectRatio,
+                               uint32_t outputImageRows,
+                               uint32_t outputImageColumns,
+                               bool (*customProcessor)(half *regularlyProcessedImage),
+                               bool display) {
+    this->minAspectRatio = minAspectRatio;
+    this->maxAspectRatio = maxAspectRatio;
+    this->outputImageRows = outputImageRows;
+    this->outputImageColumns = outputImageColumns;
+    this->display = display;
+    customProcessorUint8 = nullptr;
+    customProcessorHalf = customProcessor;
+    bytesPerPixel = 2;
+}
+
+uint64_t ImageProcessor::outputTensorSizeInPixels() { return 3 * outputImageRows * outputImageColumns; }
+uint64_t ImageProcessor::outputTensorSizeInBytes() { return bytesPerPixel * outputTensorSizeInPixels(); }
+
+ThorImplementation::TensorDescriptor::DataType ImageProcessor::getDataType() {
+    if (bytesPerPixel == 1)
+        return ThorImplementation::TensorDescriptor::DataType::UINT8;
+    else
+        return ThorImplementation::TensorDescriptor::DataType::FP16;
+}
 
 DataElement ImageProcessor::operator()(DataElement &input) {
     bool success;
@@ -35,14 +73,31 @@ DataElement ImageProcessor::operator()(DataElement &input) {
         return output;
 
     unique_ptr<uint8_t> data(new uint8_t[outputTensorSizeInBytes()]);
-    success = ImageLoader::toRgbArray(image, data.get(), false);
-    if (!success)
-        return output;
 
-    if (customProcessor != nullptr) {
-        bool useImage = customProcessor(image);
-        if (!useImage)
+    if (bytesPerPixel == 1) {
+        // output.dataType = ThorImplementation::TensorDescriptor::DataType::UINT8;
+
+        success = ImageLoader::toRgbArray(image, data.get(), ImageLoader::Layout::CHW);
+        if (!success)
             return output;
+
+        if (customProcessorUint8 != nullptr) {
+            bool useImage = customProcessorUint8(data.get());
+            if (!useImage)
+                return output;
+        }
+    } else {
+        // output.dataType = ThorImplementation::TensorDescriptor::DataType::FP16;
+
+        success = ImageLoader::toRgbArray(image, (half *)data.get(), ImageLoader::Layout::CHW);
+        if (!success)
+            return output;
+
+        if (customProcessorHalf != nullptr) {
+            bool useImage = customProcessorHalf((half *)data.get());
+            if (!useImage)
+                return output;
+        }
     }
 
     if (display) {
