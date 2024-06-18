f06e9f1cefb1c0f34bd87ce69376be6cb1cf806e
amrobbins <andrew.robbins@cart.com>
2024-06-18 00:59:19 -0500

    Update install_dependencies.sh

--- MachineSetup/install_dependencies.sh
+++ MachineSetup/install_dependencies.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
 sudo apt-get install -y build-essential cmake clang-format libboost-all-dev lib64ncurses-dev install libx11-dev
 sudo apt-get install -y libpng-dev zlib1g-dev libgs-dev libjpeg-dev libtiff-dev libxml2-dev
-sudo apt install -y libgraphicsmagick1-dev
+sudo apt install -y libmagick++-dev
