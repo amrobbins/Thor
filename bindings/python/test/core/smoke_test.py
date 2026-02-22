f04cebd2cb296c35ca70b61c87890762fde34444
Andrew Robbins <andrewrobbins@hotmail.com>
2026-02-21 19:12:04 -0600

    Initial wheel support. Python smoke test

---
+++ bindings/python/test/core/smoke_test.py
@@ -0,0 +1,34 @@
+from thor import Network
+from thor.layers import NetworkInput
+from thor.layers import NetworkOutput
+from thor.layers import Convolution2d
+from thor.layers import FullyConnected
+from thor import DataType
+from thor import Tensor
+
+
+def test_basic_network():
+    n = Network("smoke_network")
+    ni = NetworkInput(n, "smoke_input", [3, 100, 100], data_type=DataType.fp16)
+    conv = Convolution2d(
+        n,
+        ni.get_feature_output(),
+        1,
+        10,
+        10,
+        10,
+        1,
+    )
+    fc = FullyConnected(
+        n,
+        conv.get_feature_output(),
+        10,
+        True,
+    )
+    no = NetworkOutput(
+        n,
+        'smoke_output',
+        fc.get_feature_output(),
+        DataType.fp32,
+    )
+    t = Tensor([2, 50], data_type=DataType.uint8)
