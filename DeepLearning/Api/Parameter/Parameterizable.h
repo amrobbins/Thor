e5e8c713a365c4f8d5b7bd50d1298353f17c156c
amrobbins <andrewrobbins@hotmail.com>
2026-05-03 12:16:50 -0500

    Restore API Parameterizable surface for trainable refactor

--- DeepLearning/Api/Parameter/Parameterizable.h
+++ DeepLearning/Api/Parameter/Parameterizable.h
@@ -2,10 +2,11 @@
 
 #include "DeepLearning/Api/Parameter/BoundParameter.h"
 
+#include <algorithm>
 #include <cstdint>
-#include <debug/unordered_map>
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 namespace Thor {
@@ -17,17 +18,20 @@ class Parameterizable {
    public:
     virtual ~Parameterizable() = default;
 
-    Parameterizable(const uint64_t owningLayerId) : owningLayerId(owningLayerId) {};
+    Parameterizable() : owningLayerId(0) {}
+    explicit Parameterizable(const uint64_t owningLayerId) : owningLayerId(owningLayerId) {}
 
     void addParameter(const std::shared_ptr<ParameterSpecification>& parameter);
     std::vector<std::string> listParameters() const;
     virtual bool hasParameter(const std::string& name) const;
-    uint64_t getOwningLayerId() const { return owningLayerId; }
+    virtual uint64_t getParameterizableId() const { return owningLayerId; }
+    uint64_t getOwningLayerId() const { return getParameterizableId(); }
 
     std::shared_ptr<ParameterSpecification> getParameterSpecification(const std::string& name) const;
+    virtual std::vector<std::shared_ptr<ParameterSpecification>> getParameters() const;
 
     BoundParameter getBoundParameter(PlacedNetwork& placedNetwork, const std::string& name) const;
-    std::vector<BoundParameter> getBoundParameter(PlacedNetwork& placedNetwork, const std::string name) const;
+    std::vector<BoundParameter> getBoundParameters(PlacedNetwork& placedNetwork) const;
 
     std::string listParametersString() const;
 
