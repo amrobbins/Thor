fd3a5bb7496ebb5b8ee4adae051f457f46a4abc6
amrobbins <andrewrobbins@hotmail.com>
2026-05-03 12:23:22 -0500

    Centralize API trainable parameter plumbing

--- DeepLearning/Api/Layers/Learning/TrainableLayer.h
+++ DeepLearning/Api/Layers/Learning/TrainableLayer.h
@@ -5,11 +5,14 @@
 #include "DeepLearning/Api/Layers/MultiConnectionLayer.h"
 #include "DeepLearning/Api/Optimizers/Optimizer.h"
 #include "DeepLearning/Api/Parameter/Parameterizable.h"
+#include "DeepLearning/Api/Parameter/ParameterSpecification.h"
 #include "DeepLearning/Implementation/Layers/TrainableLayer.h"
 
 #include <nlohmann/json.hpp>
 
-#include "DeepLearning/Api/Parameter/ParameterSpecification.h"
+#include <memory>
+#include <string>
+#include <vector>
 
 namespace Thor {
 
@@ -17,7 +20,14 @@ class TrainableLayer : public MultiConnectionLayer, public Parameterizable {
    public:
     using Layer::initialize;  // So that compiler doesn't complain about override below.
 
-    virtual ~TrainableLayer() {}
+    virtual ~TrainableLayer() = default;
+
+    uint64_t getParameterizableId() const override { return getId(); }
+
+    void attachOptimizer(std::shared_ptr<Optimizer> optimizer);
+    bool hasOptimizer() const;
+    std::shared_ptr<Optimizer> getOptimizer() const;
+    void removeOptimizer();
 
     virtual nlohmann::json architectureJson() const = 0;
     // Trainable layers cannot use serialize(thor_file::TarWriter &archiveWriter, Stream stream), they use the custom signature below.
@@ -31,29 +41,29 @@ class TrainableLayer : public MultiConnectionLayer, public Parameterizable {
     static std::unordered_map<std::string, Deserializer> &get_registry();
     static void register_layer(std::string name, Deserializer fn);
 
-    virtual std::vector<Event> initialize(std::shared_ptr<ThorImplementation::TrainableLayer> layer,
-                                          bool isFirstStamp,
-                                          std::shared_ptr<ThorImplementation::TrainableLayer> sisterLayer,
-                                          Optional<Event> sisterLayerLoadedEvent) {
-        return MultiConnectionLayer::initialize(layer);
-        // FIXME: What do I need to do here?
-    }
+    std::vector<Event> initialize(std::shared_ptr<ThorImplementation::TrainableLayer> layer,
+                                  bool isFirstStamp,
+                                  std::shared_ptr<ThorImplementation::TrainableLayer> sisterLayer,
+                                  Optional<Event> sisterLayerLoadedEvent) override;
 
    protected:
-    virtual void compile(std::shared_ptr<ThorImplementation::Layer> physicalLayer) {
-        MultiConnectionLayer::compile(physicalLayer);
-        // FIXME: What do I need to do here on the API side?
-    }
+    void compile(std::shared_ptr<ThorImplementation::Layer> physicalLayer) override;
 
-    virtual void serializeParameters(thor_file::TarWriter &archiveWriter,
-                                     Stream stream,
-                                     bool saveOptimizerState,
-                                     ThorImplementation::StampedNetwork &stampedNetwork);
+    void stampOptimizer(const std::shared_ptr<ThorImplementation::TrainableLayer> &physicalTrainableLayer) const;
+    std::shared_ptr<Optimizer> optimizerForParameter(const std::shared_ptr<ParameterSpecification> &parameter) const;
 
-    virtual void deserializeParameters(thor_file::TarReader &archiveReader,
-                                 Stream stream,
-                                 bool loadOptimizerState,
-                                 ThorImplementation::StampedNetwork &stampedNetwork);
+    void addParameterArchitectureJson(nlohmann::json &j) const;
+    void deserializeParameterArchitectureJson(const nlohmann::json &j, std::shared_ptr<thor_file::TarReader> &archiveReader);
+
+    void serializeParameters(nlohmann::json &j,
+                             thor_file::TarWriter &archiveWriter,
+                             Stream stream,
+                             bool saveOptimizerState,
+                             ThorImplementation::StampedNetwork &stampedNetwork) const;
+
+    void deserializeParameters(const nlohmann::json &j, std::shared_ptr<thor_file::TarReader> &archiveReader);
+
+    std::shared_ptr<Optimizer> optimizer;
 };
 
 }  // namespace Thor
