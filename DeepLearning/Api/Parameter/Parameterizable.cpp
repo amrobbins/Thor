1d979f3ba897c8fb73ef48e2793edb35b2c95101
amrobbins <andrewrobbins@hotmail.com>
2026-05-03 12:22:39 -0500

    Add API parameter list helpers

--- DeepLearning/Api/Parameter/Parameterizable.cpp
+++ DeepLearning/Api/Parameter/Parameterizable.cpp
@@ -4,6 +4,7 @@
 #include "DeepLearning/Api/Parameter/BoundParameter.h"
 #include "DeepLearning/Api/Parameter/ParameterSpecification.h"
 
+#include <algorithm>
 #include <stdexcept>
 
 using namespace std;
@@ -11,6 +12,10 @@ using namespace std;
 namespace Thor {
 
 void Parameterizable::addParameter(const std::shared_ptr<ParameterSpecification>& parameter) {
+    if (parameter == nullptr) {
+        throw runtime_error("Trying to add a null parameter specification.");
+    }
+
     const string paramName = parameter->getName();
     if (hasParameter(paramName)) {
         throw runtime_error("Trying to add parameter named " + paramName +
@@ -26,11 +31,24 @@ std::vector<std::string> Parameterizable::listParameters() const {
     names.reserve(parameters.size());
 
     for (const auto& [key, value] : parameters) {
+        (void)value;
         names.push_back(key);
     }
 
     std::sort(names.begin(), names.end());
     return names;
+}
+
+std::vector<std::shared_ptr<ParameterSpecification>> Parameterizable::getParameters() const {
+    std::vector<std::shared_ptr<ParameterSpecification>> result;
+    std::vector<std::string> names = listParameters();
+    result.reserve(names.size());
+
+    for (const std::string& name : names) {
+        result.push_back(getParameterSpecification(name));
+    }
+
+    return result;
 }
 
 shared_ptr<ParameterSpecification> Parameterizable::getParameterSpecification(const std::string& name) const {
@@ -42,7 +60,19 @@ shared_ptr<ParameterSpecification> Parameterizable::getParameterSpecification(co
 }
 
 BoundParameter Parameterizable::getBoundParameter(PlacedNetwork& placedNetwork, const std::string& name) const {
-    return BoundParameter(getParameterSpecification(name), &placedNetwork, getOwningLayerId());
+    return BoundParameter(getParameterSpecification(name), &placedNetwork, getParameterizableId());
+}
+
+std::vector<BoundParameter> Parameterizable::getBoundParameters(PlacedNetwork& placedNetwork) const {
+    std::vector<BoundParameter> result;
+    const std::vector<std::string> names = listParameters();
+    result.reserve(names.size());
+
+    for (const std::string& name : names) {
+        result.push_back(getBoundParameter(placedNetwork, name));
+    }
+
+    return result;
 }
 
 string Parameterizable::listParametersString() const {
@@ -55,6 +85,10 @@ string Parameterizable::listParametersString() const {
         }
 
         result += names[i];
+    }
+
+    if (result.empty()) {
+        result = "<none>";
     }
 
     return result;
