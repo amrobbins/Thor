bfe012863457e3efc12e018a3be09b984484d440
amrobbins <andrewrobbins@hotmail.com>
2026-05-04 14:16:32 -0500

    Implement ReLU activation expression

--- DeepLearning/Api/Layers/Activations/Relu.h
+++ DeepLearning/Api/Layers/Activations/Relu.h
@@ -18,6 +18,8 @@ class Relu : public Activation {
         return myClone;
     }
 
+    virtual ThorImplementation::Expression toExpression(const ThorImplementation::Expression& input) const { return input.max(0.0); }
+
     virtual std::string getLayerType() const { return "Relu"; }
 
     static void deserialize(const nlohmann::json &j, Network *network) {
