926f3048faed0817ef2c0f9774ce77798d8dfc9b
Andrew Robbins <andrewrobbins@hotmail.com>
2026-05-31 17:12:37 -0500

    Remove legact non-fused tensor math

--- DeepLearning/Implementation/Layers/Math/Arithmetic/DivideTensorByConstantScalar.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "DeepLearning/Implementation/Layers/Layer.h"
-
-namespace ThorImplementation {
-
-class DivideTensorByConstantScalar : public Layer {};
-
-}  // namespace ThorImplementation
